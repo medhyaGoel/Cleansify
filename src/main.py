645fe72187cfff5d9f8f2c5530829e63d84eb671
medhyaGoel <71662879+medhyaGoel@users.noreply.github.com>
2023-12-22 21:42:06 -0800

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,173 @@
+# This library contains functions that access spotify API to detox playlists
+import json
+import secrets
+import re
+import requests
+import base64
+from flask import request
+
+# Spotify URLS
+BASE_URL = 'https://api.spotify.com/v1/'
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+SPOTIFY_PROFILE_URL = "https://api.spotify.com/v1/me"
+
+# Server-side Parameters
+REDIRECT_URI = "http://localhost:8081/callback/q"
+SCOPE = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-public ' \
+        'playlist-modify-private'
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+
+def create_new_playlist(user_name, track_ids, token, isPublic, playlist_name):
+    # create blank playlist
+    url = f'https://api.spotify.com/v1/users/{user_name}/playlists'
+    token["Content-Type"] = "application/json"
+    body = {
+        'name': playlist_name,
+        'public': isPublic,
+        'description': "Crafted with Cleansify"
+    }
+    response = requests.post(url, headers=token, json=body)
+    playlist_id = response.json()['id']
+
+
+    # add tracks
+    track_ids = json.loads(track_ids[0])
+    track_ids = ['spotify:track:' + item for item in track_ids]
+
+    add_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
+    body = {
+        'uris': track_ids,
+        'position': 0
+    }
+    response = requests.post(add_url, headers=token, json=body)
+    return response.json()
+
+
+# Authorization of application with spotify
+def app_Authorization(client_id):
+
+    state = secrets.token_urlsafe(16)
+
+    auth_query_parameters = {
+        "response_type": "code",
+        "redirect_uri": REDIRECT_URI,
+        "client_id": client_id,
+        "scope": SCOPE,
+        "state": state
+    }
+    resp = requests.get(
+        url=SPOTIFY_AUTH_URL,
+        params=auth_query_parameters,
+        allow_redirects=True
+    )
+    return resp.url
+
+
+# User has allowed us to access their spotify
+def user_Authorization(client_id, client_secret):
+    auth_token = request.args['code']
+    if not auth_token:
+        app_Authorization(client_id)
+    else:
+        code_payload = {
+            "grant_type": "authorization_code",
+            "code": str(auth_token),
+            "redirect_uri": REDIRECT_URI
+        }
+        base64encoded = base64.b64encode((client_id + ":" + client_secret).encode("ascii")).decode("ascii")
+        headers = {'content-type': 'application/x-www-form-urlencoded',
+                   "Authorization": "Basic {}".format(base64encoded)}
+        post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
+
+        # Tokens are Returned to Application
+        response_data = json.loads(post_request.text)
+        access_token = response_data["access_token"]
+
+        # Use the access token to access Spotify API
+        authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+        return authorization_header
+
+
+# grab the playlist at the given url
+def grab_playlist_by_url(playlist_url, header):
+    match = re.search(r'playlist/([a-zA-Z0-9_-]+)', playlist_url)
+    playlist_id = match.group(1) if match else None
+    PLAYLIST_URL = 'https://api.spotify.com/v1/playlists/' + playlist_id
+    playlist_response = requests.get(PLAYLIST_URL, headers=header)
+    return playlist_response.json()
+
+
+def fetch_profile(headers):
+    response = requests.get(SPOTIFY_PROFILE_URL, headers=headers)
+    return response.json()
+
+
+# create clean version of playlist
+def clean_up_list(header, playlist):
+    clean_versions = []
+    # collect clean_versions
+    for track in playlist['tracks']['items']:
+        # if track is clean, add it to clean_versions
+        if not track['track']['explicit']:
+            track['track']['certainty'] = "Original"
+            clean_versions.append(track['track'])
+        else:
+            # search for clean version
+            title_string = '+'.join(track['track']['name'].split())
+            artist_string = ""
+            if track['track']['artists'] is not None:
+                artist_string = "artist%3D"
+                for artist in track['track']['artists']:
+                    artist_string += '+'.join(artist['name'].split())
+
+            SEARCH_URL = 'https://api.spotify.com/v1/search?q=' + title_string + '+clean%2C+' + artist_string + '&type=track'
+            search_results = requests.get(SEARCH_URL, headers=header).json()
+            # iterate through search results and append first result marked not explicit
+            isAdded = False
+            for result in search_results['tracks']['items']:
+                if not result['explicit']:
+                    clean_versions.append(result)
+                    result['certainty'] = 'Potential swap'
+                    isAdded = True
+                    break
+            if not isAdded:
+                track['track']['certainty'] = 'No match'
+                clean_versions.append(track['track'])
+
+    return clean_versions
+
+
+# grab all the playlists of the given user_id
+def get_user_playlists(profile, headers):
+    url = f'https://api.spotify.com/v1/users/{profile["id"]}/playlists'
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+def authorize(client_id, client_secret):
+    # POST
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret,
+    })
+
+    # convert the response to JSON
+    auth_response_data = auth_response.json()
+
+    # save the access token
+    access_token = auth_response_data['access_token']
+    print(f"access token is {access_token}")
+
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=access_token)
+    }
+    return headers
---
+++ src/server.py
@@ -0,0 +1,132 @@
+import copy
+import os
+from datetime import timedelta
+from flask import Flask, redirect, render_template, session, url_for, jsonify
+from flask_cors import CORS
+from main import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("APP_SECRET")
+CORS(app)
+
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+# Set session timeout to 30 minutes
+app.permanent_session_lifetime = timedelta(minutes=30)
+
+
+# Session hygiene.
+@app.route('/clear_session', methods=['GET'])
+def clear_session():
+    # Clear session data
+    session.clear()
+    return 'Session cleared', 200
+
+
+# login to Spotify. Redirects to callback/q.
+@app.route("/login")
+def index():
+    # Authorization
+    response = app_Authorization(CLIENT_ID)
+    return redirect(response)
+
+
+# stores authorization token in session and redirects to homepage.
+@app.route("/callback/q")
+def callback():
+    # verifier = request.args.get('verifier')
+    authorization_header = user_Authorization(CLIENT_ID, CLIENT_SECRET)
+    session['spotify_token'] = authorization_header
+    return redirect(url_for('hello_world'))
+
+
+@app.route('/url', methods=['GET'])
+def get_playlist_url():
+    playlist_id = request.args.get('playlist_id')
+    # return jsonify(playlist_id)
+    headers = authorize(CLIENT_ID, CLIENT_SECRET)
+    playlist_data = grab_playlist_by_url(playlist_id, headers)
+    return jsonify(playlist_data)
+
+
+# returns cleaned tracks
+@app.route('/clean', methods=['GET'])
+def clean_playlist_url():
+    playlist_id = request.args.get('playlist_id')
+    # return jsonify(playlist_id)
+    access_token, headers = authorize(CLIENT_ID, CLIENT_SECRET)
+    playlist_data = grab_playlist_by_url(playlist_id, headers)
+    cleaned_data = clean_up_list(headers, playlist_data)
+    return jsonify(cleaned_data)
+
+
+# renders homepage.
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    userid = None
+    myPlaylists = None
+    clean_tracks = None
+    playlist_tracks = None
+
+    if 'spotify_token' in session:
+        my_profile = fetch_profile(session['spotify_token'])
+        userid = my_profile['display_name']
+        myPlaylists = get_user_playlists(my_profile, session['spotify_token'])
+
+    if request.method == 'POST' or request.args.get('url'):
+        playlist_url = request.form.get('url') if request.method == 'POST' else request.args.get('url')
+        if 'spotify_token' not in session:
+            access_token, headers = authorize(CLIENT_ID, CLIENT_SECRET)
+        else:
+            headers = session['spotify_token']
+        playlist_tracks = grab_playlist_by_url(playlist_url, headers)
+        cleaned_tracks = copy.deepcopy(playlist_tracks)
+        clean_tracks = clean_up_list(headers, cleaned_tracks)
+
+    return render_template("homePage.html", tracks=playlist_tracks, cleaned=clean_tracks, userPlaylists=myPlaylists,
+                           userId=userid)
+
+
+# create a playlist containing the cleaned tracks. User can specify name of playlist and whether it is public or private.
+# Redirects to homepage.
+@app.route("/cleansify", methods=['POST'])
+def create_playlist():
+    if request.method == 'POST':
+        track_ids = request.form.getlist('track_ids')
+        playlist_name = request.form.get('playlist_name')
+        isPublic = False if request.form.get('playlist_type') == "private_playlist" else True
+        my_profile = fetch_profile(session['spotify_token'])
+        user_name = my_profile['id']
+        create_new_playlist(user_name, track_ids, session['spotify_token'], isPublic, playlist_name)
+    return redirect(url_for('hello_world'))
+
+
+# Filter to change the color cleaned songs depending on how likely the located clean version of the track is accurate.
+def certainty_color(level):
+    if level == 'Potential swap':
+        return '#FFA726'
+    elif level == 'No match':
+        return '#BF360C'
+    else:
+        return '#181818'
+
+
+app.jinja_env.filters['certainty_color'] = certainty_color
+
+
+# Log user out of spotify account and redirect to homepage.
+@app.route('/logout')
+def logout():
+    session.pop('spotify_token', None)
+    return redirect(url_for('hello_world'))
+
+
+# main()
+# _______________
+# Setting up host and port
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8081)
